03765dc787ff3cc5a4234e64bc52a7619504d246
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-17 11:44:36 -0500

    add visemes mapping

---
+++ frontend/src/constants/visemesMapping.js
@@ -0,0 +1,13 @@
+const visemesMapping = {
+  A: "viseme_PP",
+  B: "viseme_kk",
+  C: "viseme_I",
+  D: "viseme_AA",
+  E: "viseme_O",
+  F: "viseme_U",
+  G: "viseme_FF",
+  H: "viseme_TH",
+  X: "viseme_PP",
+};
+
+export default visemesMapping;
