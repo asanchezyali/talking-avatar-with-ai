259e15df9279f3629ff46fd8622ece3a66658525
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-17 11:45:33 -0500

    update scenario component

--- frontend/src/components/Sceneario.jsx
+++ frontend/src/components/Sceneario.jsx
@@ -2,35 +2,7 @@ import { CameraControls, ContactShadows, Environment, Text } from "@react-three/
 import { Suspense, useEffect, useRef, useState } from "react";
 import { useChat } from "../hooks/useChat";
 import { Avatar } from "./Avatar";
-
-const LoadingDots = (props) => {
-  const { loading } = useChat();
-  const [loadingText, setLoadingText] = useState("");
-  useEffect(() => {
-    if (loading) {
-      const interval = setInterval(() => {
-        setLoadingText((loadingText) => {
-          if (loadingText.length > 2) {
-            return ".";
-          }
-          return loadingText + ".";
-        });
-      }, 800);
-      return () => clearInterval(interval);
-    } else {
-      setLoadingText("");
-    }
-  }, [loading]);
-  if (!loading) return null;
-  return (
-    <group {...props}>
-      <Text fontSize={0.14} anchorX={"left"} anchorY={"bottom"}>
-        {loadingText}
-        <meshBasicMaterial attach="material" color="black" />
-      </Text>
-    </group>
-  );
-};
+import { LoadingDots } from "./LoadingDots";
 
 export const Scenario = () => {
   const cameraControls = useRef();
