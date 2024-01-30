f80d83fc2c1ef25b5f9e1f5e4b7cdcdcc88d7d77
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-30 18:04:20 -0500

    update server

--- backend/index.js
+++ backend/index.js
@@ -1,59 +1,22 @@
-import { exec } from "child_process";
 import cors from "cors";
 import dotenv from "dotenv";
-import ElevenLabs from "elevenlabs-node";
 import express from "express";
-import { promises as fs } from "fs";
-import OpenAI from "openai";
-import { chain, parser } from "./modules/openAI.mjs";
+import { openAIChain, parser } from "./modules/openAI.mjs";
+import { readJsonTranscript, audioFileToBase64 } from "./modules/utils.mjs";
+import { lipSync } from "./modules/lip-sync.mjs";
 dotenv.config();
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "-" });
-
+const openaiApiKey = process.env.OPENAI_API_KEY;
 const elevenLabsApiKey = process.env.ELEVEN_LABS_API_KEY;
-const voiceID = "pNInz6obpgDQGcFmaJgB";
-
-const voice = new ElevenLabs({
-  apiKey: elevenLabsApiKey,
-  voiceId: voiceID,
-});
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.get("/voices", async (req, res) => {
   res.send(await voice.getVoices(elevenLabsApiKey));
 });
-
-const execCommand = (command) => {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) reject(error);
-      resolve(stdout);
-    });
-  });
-};
-
-const lipSyncMessage = async (message) => {
-  const time = new Date().getTime();
-  console.log(`Starting conversion for message ${message}`);
-  await execCommand(
-    `ffmpeg -y -i audios/message_${message}.mp3 audios/message_${message}.wav`
-    // -y to overwrite the file
-  );
-  console.log(`Conversion done in ${new Date().getTime() - time}ms`);
-  await execCommand(
-    `./bin/rhubarb -f json -o audios/message_${message}.json audios/message_${message}.wav -r phonetic`
-  );
-  // -r phonetic is faster but less accurate
-  console.log(`Lip sync done in ${new Date().getTime() - time}ms`);
-};
 
 app.post("/chat", async (req, res) => {
   const userMessage = req.body.message;
@@ -65,20 +28,20 @@ app.post("/chat", async (req, res) => {
           audio: await audioFileToBase64("audios/intro_0.wav"),
           lipsync: await readJsonTranscript("audios/intro_0.json"),
           facialExpression: "smile",
-          animation: "Talking-1",
+          animation: "TalkingOne",
         },
         {
           text: "I missed you so much... Please don't go for so long!",
           audio: await audioFileToBase64("audios/intro_1.wav"),
           lipsync: await readJsonTranscript("audios/intro_1.json"),
           facialExpression: "sad",
-          animation: "Talking-2",
+          animation: "TalkingTwo",
         },
       ],
     });
     return;
   }
-  if (!elevenLabsApiKey || openai.apiKey === "-") {
+  if (!elevenLabsApiKey || openaiApiKey === "-") {
     res.send({
       messages: [
         {
@@ -86,86 +49,27 @@ app.post("/chat", async (req, res) => {
           audio: await audioFileToBase64("audios/api_0.wav"),
           lipsync: await readJsonTranscript("audios/api_0.json"),
           facialExpression: "angry",
-          animation: "Talking-3",
+          animation: "TalkingThree",
         },
         {
           text: "You don't want to ruin Wawa Sensei with a crazy ChatGPT and ElevenLabs bill, right?",
           audio: await audioFileToBase64("audios/api_1.wav"),
           lipsync: await readJsonTranscript("audios/api_1.json"),
           facialExpression: "smile",
-          animation: "Rapping",
+          animation: "Angry",
         },
       ],
     });
     return;
   }
-  let messages = await chain.invoke({ question: userMessage, format_instructions: parser.getFormatInstructions() });
-  console.log(messages);
-
-  const completion = await openai.chat.completions.create({
-    model: process.env.OPENAI_MODEL,
-    max_tokens: 1000,
-    temperature: 0.6,
-    messages: [
-      {
-        role: "system",
-        content: `
-          You are a virtual girlfriend.
-          You will always reply with a JSON array of messages. With a maximum of 3 messages.
-          Each message has a text, facialExpression, and animation property.
-          The different facial expressions are: smile, sad, angry, surprised, funnyFace, and default.
-          The different animations are: Talking-1, Talking-2, Talking-3, Thriller and Rapping. 
-        `,
-      },
-      {
-        role: "user",
-        content: userMessage || "Hello",
-      },
-    ],
+  let messages = await openAIChain.invoke({
+    question: userMessage,
+    format_instructions: parser.getFormatInstructions(),
   });
-/*   console.log(completion.choices[0].message.content);
-  let messages = JSON.parse(completion.choices[0].message.content);
-  */
-  if (messages.messages) {
-    messages = messages.messages;
-  }
-  for (let i = 0; i < messages.length; i++) {
-    const message = messages[i];
-    console.log(message);
-    const fileName = `audios/message_${i}.mp3`;
-    const textInput = message.text;
-    await voice.textToSpeech({
-      // Required Parameters
-      fileName: fileName,
-      textInput: textInput,
-
-      // Optional Parameters
-      voiceId: "21m00Tcm4TlvDq8ikWAM",
-      stability: 0.5,
-      similarityBoost: 0.5,
-      modelId: "eleven_multilingual_v2",
-      style: 1,
-      speakerBoost: true,
-    });
-    // generate lipsync
-    await lipSyncMessage(i);
-    message.audio = await audioFileToBase64(fileName);
-    message.lipsync = await readJsonTranscript(`audios/message_${i}.json`);
-  }
-
-  res.send({ messages });
+  messages = await lipSync({ messages: messages.messages });
+  res.send({ messages: messages });
 });
 
-const readJsonTranscript = async (file) => {
-  const data = await fs.readFile(file, "utf8");
-  return JSON.parse(data);
-};
-
-const audioFileToBase64 = async (file) => {
-  const data = await fs.readFile(file);
-  return data.toString("base64");
-};
-
 app.listen(port, () => {
-  console.log(`Virtual Girlfriend listening on port ${port}`);
+  console.log(`Jack are listening on port ${port}`);
 });
