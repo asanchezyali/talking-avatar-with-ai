cdd644f959f1a056dbbe363d9a05a0be14a584ec
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-30 18:06:57 -0500

    add STT provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChatProvider } from "./hooks/useChat";
+import { STTProvider } from "./hooks/useSTT";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChatProvider>
-      <App />
-    </ChatProvider>
+    <STTProvider>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </STTProvider>
   </React.StrictMode>
 );
