1d450f43146e991035d3e31344ade89d4730a380
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-30 18:05:01 -0500

    add utils

---
+++ backend/modules/utils.mjs
@@ -0,0 +1,23 @@
+import { exec } from "child_process";
+import { promises as fs } from "fs";
+
+const execCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) reject(error);
+      resolve(stdout);
+    });
+  });
+};
+
+const readJsonTranscript = async (file) => {
+  const data = await fs.readFile(file, "utf8");
+  return JSON.parse(data);
+};
+
+const audioFileToBase64 = async (file) => {
+  const data = await fs.readFile(file);
+  return data.toString("base64");
+};
+
+export { execCommand, readJsonTranscript, audioFileToBase64 };
