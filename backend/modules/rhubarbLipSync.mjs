602d26f482812d94f4d870012320220d4a1e8658
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-30 18:04:53 -0500

    add rhubarb lip-sync

---
+++ backend/modules/rhubarbLipSync.mjs
@@ -0,0 +1,18 @@
+import { execCommand } from "./utils.mjs";
+
+const getPhonemes = async (message) => {
+  const time = new Date().getTime();
+  console.log(`Starting conversion for message ${message}`);
+  await execCommand(
+    `ffmpeg -y -i audios/message_${message}.mp3 audios/message_${message}.wav`
+    // -y to overwrite the file
+  );
+  console.log(`Conversion done in ${new Date().getTime() - time}ms`);
+  await execCommand(
+    `./bin/rhubarb -f json -o audios/message_${message}.json audios/message_${message}.wav -r phonetic`
+  );
+  // -r phonetic is faster but less accurate
+  console.log(`Lip sync done in ${new Date().getTime() - time}ms`);
+};
+
+export { getPhonemes };
