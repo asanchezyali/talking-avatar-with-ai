7a99766776be02a6c335c788d4203994708e4659
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-30 18:03:50 -0500

    add eleven labs

---
+++ backend/modules/elevenLabs.mjs
@@ -0,0 +1,26 @@
+import ElevenLabs from "elevenlabs-node";
+import dotenv from "dotenv";
+dotenv.config();
+
+const elevenLabsApiKey = process.env.ELEVEN_LABS_API_KEY;
+const voiceID = process.env.ELEVEN_LABS_VOICE_ID;
+
+const voice = new ElevenLabs({
+  apiKey: elevenLabsApiKey,
+  voiceId: voiceID,
+});
+
+async function convertTextToSpeech({ text, fileName }) {
+  await voice.textToSpeech({
+    fileName: fileName,
+    textInput: text,
+    voiceId: process.env.ELEVEN_LABS_VOICE_ID,
+    stability: 0.5,
+    similarityBoost: 0.5,
+    modelId: process.env.ELEVEN_LABS_MODEL_ID,
+    style: 1,
+    speakerBoost: true,
+  });
+}
+
+export { convertTextToSpeech };
