2ed6bd1e335914ff80c1a2c648d1f0b79cdad323
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-30 18:04:36 -0500

    update openai service

--- backend/modules/openAI.mjs
+++ backend/modules/openAI.mjs
@@ -2,6 +2,7 @@ import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StructuredOutputParser } from "langchain/output_parsers";
 import { z } from "zod";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -11,9 +12,9 @@ const template = `
   \n{format_instructions}.
   Each message has properties for text, facialExpression, and animation.
   The different facial expressions are: smile, sad, angry, surprised, funnyFace, and default.
-  The different animations are: Talking-1, Talking-2, Talking-3, Thriller, and Rapping.
+  The different animations are: Idle, TalkingOne, TalkingThree, SadIdle, Defeated, Angry, 
+  Surprised, DismissingGesture, Thinking, and ThoughtfulHeadShake.
 `;
-
 
 const prompt = ChatPromptTemplate.fromMessages([
   ["ai", template],
@@ -30,22 +31,23 @@ const parser = StructuredOutputParser.fromZodSchema(
   z.object({
     messages: z.array(
       z.object({
-        text: z.string().describe("text to be spoken by the AI"),
+        text: z.string().describe("Text to be spoken by the AI"),
         facialExpression: z
           .string()
           .describe(
-            "facial expression to be used by the AI. Select from: smile, sad, angry, surprised, funnyFace, and default"
+            "Facial expression to be used by the AI. Select from: smile, sad, angry, surprised, funnyFace, and default"
           ),
         animation: z
           .string()
           .describe(
-            "animation to be used by the AI. Select from: Talking-1, Talking-2, Talking-3, Thriller and Rapping"
+            `Animation to be used by the AI. Select from: Idle, TalkingOne, TalkingThree, SadIdle, 
+            Defeated, Angry, Surprised, DismissingGesture, Thinking, and ThoughtfulHeadShake.`
           ),
       })
     ),
   })
 );
 
-const chain = prompt.pipe(model).pipe(parser);
+const openAIChain = prompt.pipe(model).pipe(parser);
 
-export { chain, parser };
+export { openAIChain, parser };
