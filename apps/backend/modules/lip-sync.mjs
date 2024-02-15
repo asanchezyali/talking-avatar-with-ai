fc6375939fe51dbfd997cb14bca07cd092bf4c9e
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-02-14 22:35:01 -0500

    fix lip-sync

--- apps/backend/modules/lip-sync.mjs
+++ apps/backend/modules/lip-sync.mjs
@@ -5,24 +5,7 @@ import { readJsonTranscript, audioFileToBase64 } from "../utils/files.mjs";
 const MAX_RETRIES = 10;
 const RETRY_DELAY = 2000;
 
-const lipSync = async ({  }) => {
-  const messages = [
-    {
-      text: 'Drawbugs are fascinating creatures I encountered in my travels. They have the unique ability to sketch their surroundings with their legs, creating intricate designs in the sand.',
-      facialExpression: 'smile',
-      animation: 'TalkingOne'
-    },
-    {
-      text: "They're not dangerous, but they are very shy. If you're lucky enough to spot one, you have to stay very still and quiet, or they'll scurry away.",
-      facialExpression: 'default',
-      animation: 'TalkingThree'
-    },
-    {
-      text: "I've tried to sketch like them, but I can't quite get the hang of it. I guess it's a skill that only drawbugs have!",
-      facialExpression: 'funnyFace',
-      animation: 'Idle'
-    }
-  ]
+const lipSync = async ({ messages }) => {
   await Promise.all(
     messages.map(async (message, index) => {
       const fileName = `audios/message_${index}.mp3`;
