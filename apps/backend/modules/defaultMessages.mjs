ae07688240416b1ffced84dce671d757d015ad55
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-06-20 18:01:01 -0500

    update default messages

--- apps/backend/modules/defaultMessages.mjs
+++ apps/backend/modules/defaultMessages.mjs
@@ -6,48 +6,53 @@ const openAIApiKey = process.env.OPENAI_API_KEY;
 const elevenLabsApiKey = process.env.ELEVEN_LABS_API_KEY;
 
 async function sendDefaultMessages({ userMessage }) {
+  let messages;
   if (!userMessage) {
-    res.send({
-      messages: [
-        {
-          text: "Hey there... How was your day?",
-          audio: await audioFileToBase64({ fileName: "audios/intro_0.wav" }),
-          lipsync: await readJsonTranscript({ fileName: "audios/intro_0.json" }),
-          facialExpression: "smile",
-          animation: "TalkingOne",
-        },
-        {
-          text: "I'm Jack, your personal AI assistant. I'm here to help you with anything you need.",
-          audio: await audioFileToBase64({ fileName: "audios/intro_1.wav" }),
-          lipsync: await readJsonTranscript({ fileName: "audios/intro_1.json" }),
-          facialExpression: "smile",
-          animation: "TalkingTwo",
-        },
-      ],
-    });
-    return true;
+    messages = [
+      {
+        text: "Hey there... How was your day?",
+        audio: await audioFileToBase64({ fileName: "audios/intro_0.wav" }),
+        lipsync: await readJsonTranscript({ fileName: "audios/intro_0.json" }),
+        facialExpression: "smile",
+        animation: "TalkingOne",
+      },
+      {
+        text: "I'm Jack, your personal AI assistant. I'm here to help you with anything you need.",
+        audio: await audioFileToBase64({ fileName: "audios/intro_1.wav" }),
+        lipsync: await readJsonTranscript({ fileName: "audios/intro_1.json" }),
+        facialExpression: "smile",
+        animation: "TalkingTwo",
+      },
+    ];
+    return messages;
   }
   if (!elevenLabsApiKey || !openAIApiKey) {
-    res.send({
-      messages: [
-        {
-          text: "Please my friend, don't forget to add your API keys!",
-          audio: await audioFileToBase64({ fileName: "audios/api_0.wav" }),
-          lipsync: await readJsonTranscript({ fileName: "audios/api_0.json" }),
-          facialExpression: "angry",
-          animation: "TalkingThree",
-        },
-        {
-          text: "You don't want to ruin Jack with a crazy ChatGPT and ElevenLabs bill, right?",
-          audio: await audioFileToBase64({ fileName: "audios/api_1.wav" }),
-          lipsync: await readJsonTranscript({ fileName: "audios/api_1.json" }),
-          facialExpression: "smile",
-          animation: "Angry",
-        },
-      ],
-    });
-    return true;
+    messages = [
+      {
+        text: "Please my friend, don't forget to add your API keys!",
+        audio: await audioFileToBase64({ fileName: "audios/api_0.wav" }),
+        lipsync: await readJsonTranscript({ fileName: "audios/api_0.json" }),
+        facialExpression: "angry",
+        animation: "TalkingThree",
+      },
+      {
+        text: "You don't want to ruin Jack with a crazy ChatGPT and ElevenLabs bill, right?",
+        audio: await audioFileToBase64({ fileName: "audios/api_1.wav" }),
+        lipsync: await readJsonTranscript({ fileName: "audios/api_1.json" }),
+        facialExpression: "smile",
+        animation: "Angry",
+      },
+    ];
+    return messages;
   }
 }
 
-export { sendDefaultMessages };
+const defaultResponse = [
+  {
+    text: "I'm sorry, there seems to be an error with my brain, or I didn't understand. Could you please repeat your question?",
+    facialExpression: "sad",
+    animation: "Idle",
+  },
+];
+
+export { sendDefaultMessages, defaultResponse };
--- apps/backend/server.js
+++ apps/backend/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import { openAIChain, parser } from "./modules/openAI.mjs";
 import { lipSync } from "./modules/lip-sync.mjs";
-import { sendDefaultMessages } from "./modules/defaultMessages.mjs";
+import { sendDefaultMessages, defaultResponse } from "./modules/defaultMessages.mjs";
 import { convertAudioToText } from "./modules/whisper.mjs";
 
 dotenv.config();
@@ -21,11 +21,20 @@ app.get("/voices", async (req, res) => {
 
 app.post("/tts", async (req, res) => {
   const userMessage = await req.body.message;
-  if (await sendDefaultMessages({ userMessage })) return;
-  let openAImessages = await openAIChain.invoke({
-    question: userMessage,
-    format_instructions: parser.getFormatInstructions(),
-  });
+  const defaultMessages = await sendDefaultMessages({ userMessage });
+  if (defaultMessages) {
+    res.send({ messages: defaultMessages });
+    return;
+  }
+  let openAImessages;
+  try {
+    openAImessages = await openAIChain.invoke({
+      question: userMessage,
+      format_instructions: parser.getFormatInstructions(),
+    });
+  } catch (error) {
+    openAImessages = defaultResponse;
+  }
   openAImessages = await lipSync({ messages: openAImessages.messages });
   res.send({ messages: openAImessages });
 });
@@ -34,10 +43,15 @@ app.post("/sts", async (req, res) => {
   const base64Audio = req.body.audio;
   const audioData = Buffer.from(base64Audio, "base64");
   const userMessage = await convertAudioToText({ audioData });
-  let openAImessages = await openAIChain.invoke({
-    question: userMessage,
-    format_instructions: parser.getFormatInstructions(),
-  });
+  let openAImessages;
+  try {
+    openAImessages = await openAIChain.invoke({
+      question: userMessage,
+      format_instructions: parser.getFormatInstructions(),
+    });
+  } catch (error) {
+    openAImessages = defaultResponse;
+  }
   openAImessages = await lipSync({ messages: openAImessages.messages });
   res.send({ messages: openAImessages });
 });
